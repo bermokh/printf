87571467ce5bcdc9d5acff1ee5036acad5808194
bermokh <berramimokhtara@gmail.com>
2023-05-29 06:01:26 -0700

    print a char, string, %

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/*FLAGS */
+#define F_MINUS 1
+#define F_ZERO 4
+/* FUNCTION */
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+
+#endif
